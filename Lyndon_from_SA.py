34bf488fe683b3d6c2301816610785116766f7a8
Ivan Radonov <39803187+ikar1234@users.noreply.github.com>
2019-06-23 18:58:41 +0200

    Create Lyndon_from_SA.py

---
+++ Lyndon_from_SA.py
@@ -0,0 +1,28 @@
+# Suffix array
+posfast = lambda string: tuple(sorted(range(len(string)), key=lambda x: string[x:]))
+
+# Suffix permutation array
+def rank(string):
+    p = posfast(string)
+    r = np.zeros(len(string),dtype=np.int32)
+    for i in range(len(string)):
+        r[p[i]] = i
+    return r
+
+
+def lyndon_from_sa(x):
+    """
+    :param x: input string
+    :return: starting positions the factors
+    """
+    # use suffix permutation array (inverse of SA) for fast comparison of suffixes
+    r = rank(x)
+    p = posfast(x)
+    f = r[0]
+    l = [p[f]]
+    for i in r:
+        if i<f:
+            f=i
+            l.append(p[f])
+    # starting positions of the factors
+    return l
